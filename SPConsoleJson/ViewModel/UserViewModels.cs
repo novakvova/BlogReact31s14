664beade63efb6fce77b26d439c412b6261c669f
novakvova <novakvova@gmail.com>
2019-04-06 18:31:39 +0300

    Complete Test Storage

---
+++ SPConsoleJson/ViewModel/UserViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SPConsoleJson.ViewModel
+{
+    public class UserViewModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public bool UserSex { get; set; }
+        public string UserEmail { get; set; }
+        public int UserAge { get; set; }
+        public List<UserImageViewModel> Images { get; set; }
+    }
+
+    public class UserImageViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
