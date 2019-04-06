527194b9dd468312e7019fb551bcdd0828cdac4e
novakvova <novakvova@gmail.com>
2019-04-06 15:58:02 +0300

    Add Project work Procedure

---
+++ SPConsoleJson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SPConsoleJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
