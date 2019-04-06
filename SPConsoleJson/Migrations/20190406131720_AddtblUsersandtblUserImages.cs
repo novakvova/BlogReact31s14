26f82c06cee2ecd6a0b703efcae0a3429d057620
novakvova <novakvova@gmail.com>
2019-04-06 16:58:59 +0300

    Add-Database

---
+++ SPConsoleJson/Migrations/20190406131720_AddtblUsersandtblUserImages.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SPConsoleJson.Migrations
+{
+    public partial class AddtblUsersandtblUserImages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(maxLength: 75, nullable: false),
+                    FirstName = table.Column<string>(maxLength: 75, nullable: false),
+                    LastName = table.Column<string>(maxLength: 75, nullable: false),
+                    Age = table.Column<int>(nullable: false),
+                    Sex = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblUserImages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 75, nullable: false),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblUserImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tblUserImages_tblUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "tblUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblUserImages_UserId",
+                table: "tblUserImages",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblUserImages");
+
+            migrationBuilder.DropTable(
+                name: "tblUsers");
+        }
+    }
+}
